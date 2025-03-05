508f900d3b812be77b93b3b1327b9527ea4a0018
Antonino Morana <amorana1508@gmail.com>
2025-03-05 17:35:01 +0000

    Proyecto terminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
-import "bootstrap";
-import "./style.css";
-
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const who = ['El perro', 'Mi abuela', 'El cartero', 'Mi pájaro'];
+const action = ['se comió', 'hizo pis en', 'aplastó', 'rompió'];
+const what = ['mi tarea', 'mi teléfono', 'el coche'];
+const when = ['antes de la clase', 'cuando estaba durmiendo', 'mientras hacía ejercicio'];
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+    const randomWho = who[Math.floor(Math.random() * who.length)];
+    const randomAction = action[Math.floor(Math.random() * action.length)];
+    const randomWhat = what[Math.floor(Math.random() * what.length)];
+    const randomWhen = when[Math.floor(Math.random() * when.length)];
+
+    const excuse = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}.`;
+    document.getElementById('excuse').innerHTML = excuse;
 };
+
+
+
+
+
+
+
+
+
